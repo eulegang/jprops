03ce856e434222c5b39a78930ef587d865a93703
eulegang <eulegang@eulegang.dev>
2024-03-19 17:22:56 -0500

    Adding some more tests

--- src/lib.rs
+++ src/lib.rs
@@ -23,6 +23,7 @@ pub enum Error {
 /// The abstract notion of a properties file
 ///
 /// This uses a naive implementation but should be performant enough for most cases
+#[derive(Debug)]
 pub struct Properties<'bytes> {
     pairs: Vec<(Cow<'bytes, str>, Cow<'bytes, str>)>,
 }
@@ -32,9 +33,10 @@ impl<'bytes> Properties<'bytes> {
     pub fn load(mut content: &'bytes [u8]) -> Result<Self, Error> {
         let mut pairs = Vec::new();
         let mut line = 0;
+        let mut partial = None::<(&'bytes str, String)>;
 
         while !content.is_empty() {
-            let cur = match memchr::memchr2(b'\n', b'\r', content) {
+            let mut cur = match memchr::memchr2(b'\n', b'\r', content) {
                 Some(br) => {
                     let (cur, next) = content.split_at(br);
                     content = &next[1..];
@@ -50,22 +52,65 @@ impl<'bytes> Properties<'bytes> {
 
             line += 1;
 
-            if cur.is_empty() {
-                continue;
+            dbg!((line, std::str::from_utf8(cur).unwrap(), &partial));
+
+            if let Some(comment) = memchr::memchr2(b'#', b'!', cur) {
+                cur = &cur[..comment];
             }
 
-            let Some(assign) = memchr::memchr2(b'=', b':', cur) else {
-                let s = std::str::from_utf8(cur).map_err(|e| Error::InvalidUtf8(line, e))?;
+            if let Some((key, mut value)) = partial {
+                if odd_backslash(cur) {
+                    let ext = std::str::from_utf8(&cur[0..cur.len() - 1])
+                        .map_err(|e| Error::InvalidUtf8(line, e))?
+                        .trim();
 
-                return Err(Error::MalformedLine(line, s.to_string()));
-            };
+                    value.push_str(ext);
 
-            let (pre, post) = cur.split_at(assign);
+                    partial = Some((key, value));
+                } else {
+                    let ext = std::str::from_utf8(&cur[0..cur.len()])
+                        .map_err(|e| Error::InvalidUtf8(line, e))?
+                        .trim();
 
-            let key = std::str::from_utf8(pre).map_err(|e| Error::InvalidUtf8(line, e))?;
-            let value = std::str::from_utf8(&post[1..]).map_err(|e| Error::InvalidUtf8(line, e))?;
+                    value.push_str(ext);
 
-            pairs.push((Cow::Borrowed(key), Cow::Borrowed(value)));
+                    pairs.push((Cow::Borrowed(key), Cow::Owned(value)));
+                    partial = None;
+                }
+            } else {
+                if cur.is_empty() {
+                    continue;
+                }
+
+                let Some(assign) = memchr::memchr2(b'=', b':', cur) else {
+                    let s = std::str::from_utf8(cur).map_err(|e| Error::InvalidUtf8(line, e))?;
+
+                    return Err(Error::MalformedLine(line, s.to_string()));
+                };
+
+                let (pre, post) = cur.split_at(assign);
+
+                let key = std::str::from_utf8(pre)
+                    .map_err(|e| Error::InvalidUtf8(line, e))?
+                    .trim();
+
+                let value = std::str::from_utf8(&post[1..])
+                    .map_err(|e| Error::InvalidUtf8(line, e))?
+                    .trim();
+
+                if odd_backslash(post) {
+                    let mut value = value.to_string();
+                    value.pop();
+
+                    partial = Some((key, value));
+                } else {
+                    let value = std::str::from_utf8(&post[1..])
+                        .map_err(|e| Error::InvalidUtf8(line, e))?
+                        .trim();
+
+                    pairs.push((Cow::Borrowed(key), Cow::Borrowed(value)));
+                }
+            }
         }
 
         Ok(Properties { pairs })
@@ -96,4 +141,25 @@ impl<'bytes> Properties<'bytes> {
 
         res
     }
+}
+
+fn odd_backslash(line: &[u8]) -> bool {
+    let mut cnt = 0;
+
+    for ch in line.iter().rev() {
+        if *ch == b'\\' {
+            cnt += 1
+        } else {
+            break;
+        }
+    }
+
+    cnt & 1 == 1
+}
+
+#[test]
+fn test_odd_backslashh() {
+    assert!(odd_backslash(b"hello\\"));
+    assert!(!odd_backslash(b"hello\\\\"));
+    assert!(odd_backslash(b"hello\\\\\\"));
 }
--- tests/lines.rs
+++ tests/lines.rs
@@ -33,3 +33,55 @@ pub fn blank_lines() {
 
     assert_eq!(props.get("name"), vec![Cow::from("value")]);
 }
+
+#[test]
+pub fn comments() {
+    let content = b"# Hello!\nname=value\n\n";
+
+    let props = Properties::load(content).unwrap();
+
+    assert_eq!(props.len(), 1);
+
+    assert_eq!(props.get("name"), vec![Cow::from("value")]);
+}
+
+#[test]
+pub fn bang_comments() {
+    let content = b"! World!\nname=value\n\n";
+
+    let props = Properties::load(content).unwrap();
+
+    assert_eq!(props.len(), 1);
+
+    assert_eq!(props.get("name"), vec![Cow::from("value")]);
+}
+
+#[test]
+pub fn whitespace() {
+    let content = b"  name  \t  =    value  \t  ";
+
+    let props = Properties::load(content).unwrap();
+
+    assert_eq!(props.len(), 1);
+
+    dbg!(&props);
+
+    assert_eq!(props.get("name"), vec![Cow::from("value")]);
+}
+
+#[test]
+pub fn backspace_continue() {
+    let content = b"targetCities=\\\nDetroit,\\\n\t   Chicago,\\\n  Los Angeles\n";
+    // why does oracle want to target cities?
+
+    let props = Properties::load(content).unwrap();
+
+    assert_eq!(props.len(), 1);
+
+    dbg!(&props);
+
+    assert_eq!(
+        props.get("targetCities"),
+        vec![Cow::from("Detroit,Chicago,Los Angeles")]
+    );
+}
